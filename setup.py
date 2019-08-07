02d43fc4668d949946fa47e40e97bb9ff2863887
Dan Iulian Muntean <dan_iulian.muntean@stud.acs.upb.ro>
2019-08-07 16:34:16 +0300

    change package setup name

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
 from setuptools import setup
 
 dependency_links=[
-	'git+https://github.com/nemodrive/semantic_data_augmentation#egg=roadpackage',
+	'git+https://github.com/nemodrive/semantic_data_augmentation',
 ]
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='Road',
-    url='https://github.com/nemodrive/semantic_data_augmentation',
-    author='Dragos Homner',
-    author_email='dragoshomner@yahoo.com',
-    # Needed to actually package something
-    packages=['roadpackage'],
-    # Needed for dependencies
-    install_requires=['numpy'],
-    # *strongly* suggested for sharing
-    version='0.1',
-    # The license can be anything you like
-    license='ABC',
-    description='Overlay people on the road',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    name="roadpackage",
+    version="0.0.1",
+    author="Dragos",
+    author_email="draogs@example.com",
+    description="A small example package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=['roadpackag'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
